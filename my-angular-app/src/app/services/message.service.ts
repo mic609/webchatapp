c0004dab7c70ba0196bf61a224ceb1ceb77010df
mic609 <michu-2001@o2.pl>
2024-11-12 03:46:57 +0100

    changes

--- my-angular-app/src/app/services/message.service.ts
+++ my-angular-app/src/app/services/message.service.ts
@@ -9,7 +9,7 @@ import SockJS from 'sockjs-client';
   providedIn: 'root'
 })
 export class MessageService {
-  private apiUrl = 'http://44.198.109.215:8080/api/messages';
+  private apiUrl = 'http://127.0.0.1:8080/api/messages';
 
   constructor(private http: HttpClient) {}
 

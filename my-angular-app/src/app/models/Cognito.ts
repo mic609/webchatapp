e35bc3161ee5cbf8b1c6d646ed9ead450e1bd6ec
mic609 <michu-2001@o2.pl>
2024-11-15 13:47:17 +0100

    updated for user pool (cloud)

--- my-angular-app/src/app/models/Cognito.ts
+++ my-angular-app/src/app/models/Cognito.ts
@@ -1,5 +1,5 @@
 export const cognito = {
     production: false,
-    cognitoUserPoolId: 'us-east-1_mJvucceU5',
-    cognitoAppClientId: '1p7kd3q27b13q4rploq0mr66tq',
+    cognitoUserPoolId: 'us-east-1_wOPyJ5aFg',
+    cognitoAppClientId: '39rced4s1fs7p42r1s36juddk9',
 }
